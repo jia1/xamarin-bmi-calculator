ac9c365aa597129791d4c5e48d9a3f54f03968c8
Jia Yee <jiayeerawr@gmail.com>
2016-05-24 16:39:53 +0800

    Write the calculate function in the code file

---
+++ xamarin-bmi-calculator/BmiCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+    public static class BmiCalculator
+    {
+        public static string Calculate(string h, string w)
+        {
+            double height, weight, bmi;
+            if (string.IsNullOrWhiteSpace(h) || string.IsNullOrWhiteSpace(w))
+            {
+                return "Please fill in the blank(s)!";
+            }
+            else
+            {
+                height = double.Parse(h);
+                weight = double.Parse(w);
+                bmi = weight / Math.Pow(height, 2);
+                return bmi.ToString();
+            }
+        }
+    }
+}
